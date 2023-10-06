726f15b9b9d735e217d57959405a6fae5e7f8f57
Exsi7 <ZotovV777@yandex.ru>
2023-10-06 15:42:05 +0300

    v1

--- menu/tree_menu/urls.py
+++ menu/tree_menu/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from menu.tree_menu import views
-from menu.tree_menu.templatetags.menu_tags import draw_menu
+
 urlpatterns = [
     path('', views.index, name="index"),
     path("<str:url>/", views.tree_view, name="tree"),
