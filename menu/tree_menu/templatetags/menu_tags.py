cd8f7cb3f3e2b1b1b6f5814602e88b6c0156f6c4
Exsi7 <ZotovV777@yandex.ru>
2023-10-06 15:50:58 +0300

    fix menu_tags args

--- menu/tree_menu/templatetags/menu_tags.py
+++ menu/tree_menu/templatetags/menu_tags.py
@@ -4,7 +4,7 @@ from menu.tree_menu.models import Menu, MenuItem
 register = template.Library()
 
 @register.inclusion_tag("tree_menu/menu_tag.html", takes_context=True)
-def draw_menu(context, menu_name, parent_id=None, url=None):
+def draw_menu(context, menu_name, parent_id=None):
     request = context['request']
     items = MenuItem.objects.filter(menu__name=menu_name, parent=parent_id).select_related("parent")
     return {"items": items, 'request': request}
